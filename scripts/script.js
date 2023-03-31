c6be769f1beb69b2c159e87e262bd36932cb5465
j-harney <jpharney2022@gmail.com>
2023-03-31 16:10:23 +0000

    Files for Strike a Chord

---
+++ scripts/script.js
@@ -0,0 +1,16 @@
+/*  
+Author: Jack Harney
+File Name: script.js
+Date: 3/31/2023
+*/
+
+// Hamburger menu function
+
+function hamburger() {
+    var menu = document.getElementById("menu-links");
+    if (menu.style.display === "block") {
+        menu.style.display = "none";
+        } else {
+            menu.style.display = "block";
+        }
+}
