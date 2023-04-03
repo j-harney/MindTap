1f004f3c11d01338ed33a34a1a71c2ade7bf9b5a
j-harney <jpharney2022@gmail.com>
2023-04-03 03:35:01 +0000

    Files for Personal Portfolio

---
+++ scripts/script.js
@@ -0,0 +1,18 @@
+/*
+Student Name: Jack Harney
+File Name: script.js
+Today's Date: 4/2/2023
+*/
+
+//Hamburger menu function
+function menu() {
+    var navlinks = document.getElementById("nav-links");
+        var menuicon = document.getElementById("icon");
+        if (navlinks.style.display === "block") {
+            navlinks.style.display = "none";
+                menuicon.style.color = "#2a1f14";
+        } else {
+            navlinks.style.display = "block";
+                menuicon.style.color = "#f6eee4";
+        }
+}
